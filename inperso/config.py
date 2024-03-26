55abf07f6be5a5583c8a2a9121e0febd29317468
Son Pham-Ba <son.phamba@epfl.ch>
2024-03-26 16:28:20 +0100

    add docstrings

--- inperso/config.py
+++ inperso/config.py
@@ -4,6 +4,8 @@ from inperso.utils import load_yaml, update_dict_nested
 
 
 def load_config() -> dict:
+    """Load the configuration file and return it as a dictionary."""
+
     module_path = os.path.dirname(__file__)
     config_path = os.path.join(module_path, "config.yaml")
 
@@ -11,6 +13,11 @@ def load_config() -> dict:
 
 
 def add_env_variables_to_config(config: dict) -> dict:
+    """Add environment variables to the configuration dictionary.
+
+    The queried environment variables are defined in the "from_env" key of the original dictionary.
+    """
+
     from_env = config.pop("from_env", {})
     from_env = read_env_variables(from_env)
     config = update_dict_nested(config, from_env)
@@ -19,15 +26,24 @@ def add_env_variables_to_config(config: dict) -> dict:
 
 
 def read_env_variables(entry: dict) -> dict:
-    for key, value in entry.items():
-        if isinstance(value, dict):
-            entry[key] = read_env_variables(value)
-        else:
-            entry[key] = os.getenv(value, "")
+    """Replace the end values of the dictionary with the corresponding environment variables."""
 
-    return entry
+    new_entry = entry.copy()
+
+    for key, value in new_entry.items():
+        if isinstance(value, dict):
+            new_entry[key] = read_env_variables(value)
+        else:
+            new_entry[key] = os.getenv(value, "")
+
+    return new_entry
 
 
 config = load_config()
 config = add_env_variables_to_config(config)
+
+
+# Database configuration
+
 db = config["db"]
+db["url"] = f"http://{db['host']}:{db['port']}"
--- inperso/utils.py
+++ inperso/utils.py
@@ -2,11 +2,15 @@ import yaml
 
 
 def load_yaml(path: str) -> dict:
+    """Load a YAML file and return it as a dictionary."""
+
     with open(path, "r") as f:
         return yaml.safe_load(f)
 
 
 def update_dict_nested(target: dict, update: dict) -> dict:
+    """Update a dictionary with another dictionary, recursively."""
+
     for key, value in update.items():
         if isinstance(value, dict):
             current = target.get(key, {})
